7bf258b676da475c66899c223b6846a5699b89b4
Sean Collings <seanrcollings@gmail.com>
2019-07-26 11:12:34 -0600

    added alt props for images

--- src/components/skillsItems.jsx
+++ src/components/skillsItems.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SkillsItem(props) {
   return (
     <div className='skills-item'>
-      <img className='skills-item-img' src={props.img}/>
+      <img className='skills-item-img' src={props.img} alt='skill'/>
       <span className='skills-item-content'>
         {props.content}
         <div className='skills-item-time'>{props.time}</div>
