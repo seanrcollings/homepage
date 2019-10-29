f495553dc20262cc2f4b5c7f61cfaeaf00340605
Sean Collings <seanrcollings@gmail.com>
2019-10-29 09:00:47 -0600

    added react-router

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from './history'
 import Profile from './components/profile';
 import main from './stylesheets/main.scss'
 
-ReactDOM.render(<Profile />, document.getElementById('root'));
+ReactDOM.render(
+    <Router history={history}>
+        <Switch>
+            <Route path='/' exact component={Profile}/ >
+        </Switch>
+    </Router>,
+    document.getElementById('root'));
