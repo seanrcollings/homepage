ee2657c0884dba2b0307cdf481b4a399f2078af6
Sean Collings <seanrcollings@gmail.com>
2020-02-12 19:22:16 -0700

    added pisite api to projects

--- src/components/projectItem.jsx
+++ src/components/projectItem.jsx
@@ -1,30 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function ProjectItem(props) {
-  const [showOverlay, setShowOverlay] = useState(false)
+  const [showOverlay, setShowOverlay] = useState(false);
 
   const renderOverlay = () => {
     if (showOverlay) {
       return (
-        <div className='projects-item-overlay'>
-          <h3 className='projects-item-overlay-title'>{props.title}</h3>
-          <div className='projects-item-overlay-description'>{props.description}</div>
-          <div className='projects-item-overlay-resources'> • {renderResources()}</div>
-          <a href={props.repository} className='projects-item-overlay-source'>Source Code</a>
+        <div className="projects-item-overlay">
+          <h3 className="projects-item-overlay-title">{props.title}</h3>
+          <div className="projects-item-overlay-description">
+            {props.description}
+          </div>
+          <div className="projects-item-overlay-resources">
+            {" "}
+            • {renderResources()}
+          </div>
+          <a href={props.repository} className="projects-item-overlay-source">
+            Source Code
+          </a>
         </div>
-      )
+      );
     }
-  }
+  };
   const renderResources = () => {
     return props.resources.map(resource => {
-      return <span className='projects-item-resources-resource'>{resource} • </span>
-    })
-  }
+      return (
+        <span className="projects-item-resources-resource">{resource} • </span>
+      );
+    });
+  };
 
   return (
-    <a href={props.link} className={`projects-item ${showOverlay ? 'projects-item-grow' : 'projects-item-shrink'}`} onMouseOver={() => setShowOverlay(true)} onMouseLeave={() => setShowOverlay(false)}>
-      <img className='projects-item-img' alt='Project' src={props.img}/>
+    <a
+      href={props.link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`projects-item ${
+        showOverlay ? "projects-item-grow" : "projects-item-shrink"
+      }`}
+      onMouseOver={() => setShowOverlay(true)}
+      onMouseLeave={() => setShowOverlay(false)}
+    >
+      <img className="projects-item-img" alt="Project" src={props.img} />
       {renderOverlay()}
     </a>
-  )
+  );
 }
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,51 +1,61 @@
-import React from 'react'
-import ProjectItem from './projectItem';
+import React from "react";
+import ProjectItem from "./projectItem";
 
 // Images
-import pisite from '../img/projects/pisite.png';
-import blog from '../img/projects/blog.png';
-import moonshiver from '../img/projects/moonshiver.png';
-import mastermind from '../img/projects/mastermind.png'
+import pisite from "../img/projects/pisite.png";
+import pisiteapi from "../img/projects/pisiteapi.jpg";
+import blog from "../img/projects/blog.png";
+import moonshiver from "../img/projects/moonshiver.png";
+import mastermind from "../img/projects/mastermind.png";
 
 export default function Projects() {
   return (
-    <div  className='projects'>
+    <div className="projects">
       <h1>PROJECTS</h1>
-      <ProjectItem 
-        title='Pi Site' 
-        description='This is a site I built hosted on a Raspberry Pi' 
-        link='http://pisite.seanrcollings.com' 
-        img={pisite} 
-        resources={['ReactJS', 'Flask', 'SCSS', 'Apache', 'Raspberry Pi 3A']}
-        repository='https://github.com/seanrcollings/PiSite'
+      <ProjectItem
+        title="Pi Site"
+        description="Frontend for the Pi Site API"
+        link="http://pisite.seanrcollings.com"
+        img={pisite}
+        resources={["ReactJS", "SCSS", "Redux"]}
+        repository="https://github.com/seanrcollings/PiSite"
       />
 
-      <ProjectItem 
-        title='My Blog' 
-        description='A project I started to familirize myself with the Rails backend framework'
-        link='http://blog.seanrcollings.com' 
-        img={blog} 
-        resources={['ReactJS', 'Rails', 'SCSS', 'Heroku', 'Postgres']}
-        repository='https://github.com/seanrcollings/blog'
+      <ProjectItem
+        title="Pi Site API"
+        description="Resource API to report data about a Raspberry Pi"
+        link="http://api.pisite.seanrcollings.com"
+        img={pisiteapi}
+        resources={["Flask", "Nginx", "SQL Alchemy", "Raspberry Pi 3A"]}
+        repository="https://github.com/seanrcollings/PiSite"
       />
 
-      <ProjectItem 
-        title='Moonshiver' 
+      <ProjectItem
+        title="My Blog"
+        description="A project I started to familirize myself with the Rails backend framework"
+        link="http://blog.seanrcollings.com"
+        img={blog}
+        resources={["ReactJS", "Rails", "SCSS", "Heroku", "Postgres"]}
+        repository="https://github.com/seanrcollings/blog"
+      />
+
+      <ProjectItem
+        title="Moonshiver"
         description="A site I am currently building for my highschool's student film festival"
-        link='https://moonshiver.herokuapp.com' 
-        img={moonshiver} 
-        resources={['ReactJS', 'Rails', 'SCSS', 'Heroku', 'Postgres']}
-        repository='https://github.com/seanrcollings/moonshiver'
+        link="https://moonshiver.herokuapp.com"
+        img={moonshiver}
+        resources={["ReactJS", "Rails", "SCSS", "Heroku", "Postgres"]}
+        repository="https://github.com/seanrcollings/moonshiver"
       />
 
-      <ProjectItem 
-        title='Mastermind' 
+      <ProjectItem
+        title="Mastermind"
         description="A version of the Mastermind board game built in React"
-        link='http://mastermind.seanrcollings.com' 
-        img={mastermind} 
-        resources={['ReactJS', 'SCSS']}
-        repository='https://github.com/seanrcollings/mastermind'
+        link="http://mastermind.seanrcollings.com"
+        img={mastermind}
+        resources={["ReactJS", "SCSS"]}
+        repository="https://github.com/seanrcollings/mastermind"
       />
-    </div>    
-  )
+    </div>
+  );
 }
